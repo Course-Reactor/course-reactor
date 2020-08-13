179911a6b382bab4960731ed589d950e4d33359b
Ryan Coleman <me@ryuhhnn.com>
2020-08-12 22:25:02 -0500

    feat: finishes initial layout and templating

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,8 @@
-// import Link from "next/link";
+import Link from "next/link";
 import { motion } from "framer-motion";
-import { Twitter } from "../components/icons";
+import { FiTwitter, FiMail } from "react-icons/fi";
 import style from "../styles/components/layout.module.scss";
+import styleUtils from "../styles/components/utils.module.scss";
 
 const stagger = {
   animate: {
@@ -17,17 +18,48 @@ export default function Layout({ children }) {
       <main className={style.main}>{children}</main>
       <footer className={style.footer}>
         <div>
-          <span>The Course Reactor Foundation</span>
+          <span className={styleUtils.bold}>The Course Reactor Foundation</span>
           <p>Saint Louis, Missouri</p>
         </div>
         <div>
           <h3>Contact</h3>
-          <Twitter />
-          <span>Follow on Twitter!</span>
+          <ul>
+            <li>
+              <a
+                href="https://www.twitter.com/CourseReactor"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styleUtils.center}
+              >
+                <FiTwitter />
+                <span>Follow on Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:info@coursereactor.org"
+                className={styleUtils.center}
+              >
+                <FiMail />
+                <span>Email Us</span>
+              </a>
+            </li>
+          </ul>
         </div>
         <div>
           <h3>Company</h3>
-          Terms and Conditions Privacy Policy
+          <ul>
+            <li>
+              <Link href="/terms-and-conditions">
+                <a>Terms and Conditions</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/privacy-policy">
+                <a>Privacy Policy</a>
+              </Link>
+            </li>
+          </ul>
         </div>
       </footer>
     </motion.div>
