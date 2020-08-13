c10f4d0cb31ca00fb586f3af806ac946d66b99fe
Ryan Coleman <me@ryuhhnn.com>
2020-08-12 23:04:39 -0500

    feat: adds in open graph preview images and favicon

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,41 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="title" content="Course Reactor - Earn a Degree Online" />
+        <meta
+          name="description"
+          content="The Course Reactor Foundation exists to democratize education and allow people from all walks of life to pursue their interests and participate in academia."
+        />
+        {/* Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.coursereactor.org/" />
+        <meta
+          property="og:title"
+          content="Course Reactor - Earn a Degree Online"
+        />
+        <meta
+          property="og:description"
+          content="The Course Reactor Foundation exists to democratize education and allow people from all walks of life to pursue their interests and participate in academia."
+        />
+        <meta
+          property="og:image"
+          content="https://www.coursereactor.org/images/share_preview.jpg"
+        />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.coursereactor.org/" />
+        <meta
+          property="twitter:title"
+          content="Course Reactor - Earn a Degree Online"
+        />
+        <meta
+          property="twitter:description"
+          content="The Course Reactor Foundation exists to democratize education and allow people from all walks of life to pursue their interests and participate in academia."
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.coursereactor.org/images/share_preview.jpg"
+        />
       </Head>
       <Layout>
         <motion.section variants={fadeInUp} className={style.hero}>
