7cd877707d2318d5085a02900cc41e4fb8ce0fa2
Ryan Coleman <me@ryuhhnn.com>
2020-08-18 22:57:26 -0500

    refactor: improves breadcrumbs and provides home link on each page. closes #4

--- components/Breadcrumbs.js
+++ components/Breadcrumbs.js
@@ -1,19 +1,29 @@
 import { useRouter } from "next/router";
-import { FiArrowLeft } from "react-icons/fi";
+import Link from "next/link";
+import { FiArrowLeft, FiHome } from "react-icons/fi";
 import style from "../styles/components/breadcrumbs.module.scss";
 
 export default function Breadcrumbs() {
   const router = useRouter();
+  const pathArray = router.asPath.split("/");
+  const path =
+    pathArray.length > 2
+      ? pathArray.slice(0, pathArray.length - 1).join("/")
+      : "/";
 
   return (
-    <span
-      className={style.link}
-      onClick={(e) => router.back()}
-      role="link"
-      tabIndex={0}
-    >
-      <FiArrowLeft />
-      Back
-    </span>
+    <div className={style.container}>
+      <Link href={path}>
+        <a className={style.link}>
+          <FiArrowLeft />
+          Back
+        </a>
+      </Link>
+      <Link href="/">
+        <a className={style.link}>
+          Home <FiHome />
+        </a>
+      </Link>
+    </div>
   );
 }
